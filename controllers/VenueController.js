c1c8caf215bb5afab0239a3c9184f5a73f46cb83
Fatem J <fatima.aljunaid.257@gmail.com>
2024-07-04 14:58:54 +0300

    venue controllers for the normal user

---
+++ controllers/VenueController.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const { Venue } = require('../models')
+
+const GetCategories = async (req, res) => {
+  
+}
+
+const GetVenues = async(req, res) => {
+  
+}
+
+const GetVenueDetails = async (req, res) => {
+  
+}
+
+
+
+module.exports = {
+  GetCategories,
+  GetVenues,
+  GetVenueDetails
+}
--- models/index.js
+++ models/index.js
@@ -1,9 +1,12 @@
 // index.js
 const mongoose = require('mongoose')
 const userSchema = require('./User')
+const venueSchema = require('./Venue')
 
 const User = mongoose.model('User', userSchema)
+const Venue = mongoose.model('Venue', venueSchema)
 
 module.exports = {
-  User
+  User,
+  Venue
 }
