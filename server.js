eadc5a3ccc07fe83a316683e44a405f4764df016
Fatem J <fatima.aljunaid.257@gmail.com>
2024-07-04 14:27:48 +0300

    fixed the venue model

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const AuthRouter = require('./routes/AuthRouter')
-// const UserRouter = require('./routes/UserRouter')
-// const PostRouter = require('./routes/PostRouter')
 
 const PORT = process.env.PORT || 3001
 
@@ -18,8 +16,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/auth', AuthRouter)
-// app.use('/users', UserRouter)
-// app.use('/posts', PostRouter)
 
 app.use('/', (req, res) => {
   res.send(`We can do it!`)
